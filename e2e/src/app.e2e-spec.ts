ae3e4cd63c0f142e785974757e8db03a721877e6
xcm1030 <xcm1030@fidudom.fiducia.de>
2018-11-12 21:48:06 +0100

    initial commit

---
+++ e2e/src/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('workspace-project App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to okvp-zahlungsverkehr-auftrag-client-v001!');
+  });
+});
